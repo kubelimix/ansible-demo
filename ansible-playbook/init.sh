172d3ed2601dbae023742c954bb9c6e038f370f8
limix <coollimix@gmail.com>
2022-04-28 20:10:11 +0800

    更新ansible-playbook样例程序

---
+++ ansible-playbook/init.sh
@@ -0,0 +1,10 @@
+ansible-playbook -i hosts -v demo-playbook.yml --list-tasks
+
+# --list-hosts
+ansible-playbook -i hosts --private-key ~/.ssh/id_rsa sealer-playbook.yml --list-hosts
+# --list-tasks
+ansible-playbook -i hosts --private-key ~/.ssh/id_rsa sealer-playbook.yml --list-tasks
+# --list-tags
+ansible-playbook -i hosts --private-key ~/.ssh/id_rsa sealer-playbook.yml --list-tags
+# -skip-tags 跳过特定的节点
+ansible-playbook -i hosts --private-key ~/.ssh/id_rsa sealer-playbook.yml --skip-tags download
