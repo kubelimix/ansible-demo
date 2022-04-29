f467cbc57d14d5105c202e4357c24883563fef7f
limix <coollimix@gmail.com>
2022-04-29 17:12:16 +0800

    添加pre_tasks和post_tasks任务

--- ansible-playbook/test.sh
+++ ansible-playbook/test.sh
@@ -2,6 +2,8 @@ ansible-playbook -i hosts -v demo-playbook.yml --list-hosts
 ansible-playbook -i hosts -v demo-playbook.yml --list-tasks
 ansible-playbook -i hosts -v demo-playbook.yml --list-tags
 
+# --start-at-task 从指定任务开始
+ansible-playbook -i hosts --start-at-task="pre task" demo-playbook.yml -vv
 # --list-hosts
 ansible-playbook -i hosts --private-key ~/.ssh/id_rsa sealer-playbook.yml --list-hosts
 # --list-tasks
